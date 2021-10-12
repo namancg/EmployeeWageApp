e5ebfc45d4d854238c3c6b95dc8b20279ebad930
namancg <naman.c@perfios.com>
2021-10-12 17:22:59 +0530

    MODIFIED: added setters,getters and new attributes

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -1,63 +1,81 @@
+let employeeArr = [];
+let flag = true;
 class EmployeePayrollData {
 
-    constructor(...params) {
-        this.id = params[0];
-        this.name = params[1];
-        this.salary = params[2];
-        this.gender = params[3];
-        this.startDate = params[4];
-    }
-
-    get name() {return this._name; }
+    get name() { return this._name; }
     set name(name) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
-        if(nameRegex.test(name)) this._name = name;
-        else throw 'NAME IS INCORECT';
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(name))
+            this._name = name;
+        else{
+            flag = false
+            alert('Name is Incorrect');
+        } 
     }
-
-    get id() {return this._id; }
-    set id(id) {
-        let idRegex = RegExp('^\d*[1-9]\d*$')
-        if(idRegex.test(id)) this._id = id;
-        else 
-            throw 'ID IS INCORECT';
-    }
-
-    get salary() {return this._salary; }
+    get salary() { return this._salary; }
     set salary(salary) {
-        let salaryRegex = RegExp('^[0-9.]+$')
-        if(salaryRegex.test(salary)) this._salary = salary;
-        else 
-            throw 'SALARY IS INCORECT';
+        this._salary = salary;
     }
-
-    get gender() {return this._gender; }
+    get gender() { return this._gender; }
     set gender(gender) {
-        let genderRegex = RegExp('^[MFmf]{1}$')
-        if(genderRegex.test(gender)) this._gender = gender;
-        else 
-            throw 'GENDER IS INCORECT';
+        this._gender = gender;
     }
-
-    get startDate() {return this._startDate; }
+    get startDate() { return this._startDate; }
     set startDate(startDate) {
-        if(new Date() < startDate) throw 'START DATE IS INCORECT';
-        else this._startDate = startDate;
+        let thirtyDaysInMiliSec = 30*24*60*60*1000;
+        if(startDate <= new Date() &&  Date.now()-startDate < thirtyDaysInMiliSec)
+            this._startDate = startDate; 
+        else{
+            flag = false;
+            alert('Invalid date');
+        }
     }
-
-    
+    get departments() { return this._departments}
+    set departments(departments){
+        this._departments = departments;
+    }
+    get notes() { return notes}
+    set notes(notes) {
+        this._notes = notes;
+    }
     toString() {
-        const options = {year: 'numeric', month: 'long', day: 'numeric'};
-        const empDate = !this.startDate ? "not defined" : 
-                        this.startDate.toLocaleDateString("en-US", options);
-        return "id=" + this.id + ", name: "+this.name + ", salary: " + this.salary
-        + ", gender: "+this.gender+", startDate: "+empDate;
+        return "id="+ this.id +", name="+ this.name +", salary="+ this.salary+", gender="+ this.gender+", start date="+ this.startDate+", department="+this.departments;
     }
 }
 
-try {
-    let newEmployeePayrollData  = new EmployeePayrollData(1, "Terissa", 30000, 'm', new Date('2022-01-01'));
-    console.log("employeePayrollData: "+newEmployeePayrollData.toString());
-} catch (e) {
-    console.error(e);
+/*function save(){
+    const name = document.querySelector("#name");
+    let departments = new Array();
+    let markedCheckbox = document.getElementsByName('department');  
+    for (var checkbox of markedCheckbox) {  
+      if (checkbox.checked)  
+        departments.push(checkbox.value);  
+    } 
+    let markedRadioButton = document.getElementsByName('gender');
+    let gender;
+    for (var radiobutton of markedRadioButton) {  
+        if (radiobutton.checked)  
+          gender = radiobutton.value;  
+    }
+    const salary = document.querySelector('#salary');
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+    const date = new Date(day.value+"/"+month.value+"/"+year.value);
+    const notes = document.querySelector('#notes'); 
+    let empObject = new EmployeePayrollData();
+    flag = true;
+    empObject.name = name.value;
+    empObject.salary = salary.value;
+    empObject.gender = gender;
+    empObject.startDate = date;
+    empObject.departments = departments;
+    empObject.notes = notes.value;
+    if(flag) {
+        employeeArr.push(empObject);
+        alert('Added employee');
+    } 
+    alert(employeeArr);
 }
+console.log(employeeArr); 
+*/
