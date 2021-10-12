0c2a7943b5a60792512f899c4d3b2524f6ba8c20
namancg <naman.c@perfios.com>
2021-10-12 23:48:49 +0530

    ADDED: function to populate payroll object

--- js/EmployeeWage.js
+++ js/EmployeeWage.js
@@ -1,179 +1,98 @@
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOUR = 4;
-const FULL_TIME_HOUR = 8;
-const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 160;
-
-{
-    const IS_ABSENT = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 2
-    if(empCheck == IS_ABSENT) {
-        console.log('Employee is Absent');
-    }
-    else
-        console.log('Employee is Present');
-}
-
-function getWorkingHours(empCheck) {
-    switch (empCheck) {
-        case IS_PART_TIME:
-           return PART_TIME_HOUR;
-            break;
-        case IS_FULL_TIME:  
-            return FULL_TIME_HOUR;
-            break;
-        default:
-            return 0;
-    }
-}
-
-
-function calculateWage(empHrs) {
-    return WAGE_PER_HOUR * empHrs;
-}
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-let empDailyWageArr = new Array();
-let employeeDailyWageMap = new Map();
-let empDailyHrsMap = new Map();
-let empDailyHrsAndWageArray = new Array();
-
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    totalWorkingDays ++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    totalEmpHrs += empHrs;
-    empDailyWageArr.push(calculateWage(empHrs))
-    employeeDailyWageMap.set(totalWorkingDays,calculateWage(empHrs))
-    empDailyHrsAndWageArray.push({
-        dayNum: totalWorkingDays,
-        dailyHours: empHrs,
-        dailyWage: calculateWage(empHrs),
-        toString() {
-            return '\nDay: '+this.dayNum+' => Working Hours: '+this.dailyHours +
-                    ' And daily wage: '+this.dailyWage;
-        }
+window.addEventListener('DOMContentLoaded', (event) =>{
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+      if(name.value.length == 0){
+        textError.textContent = "";
+        return;
+      }
+      try{
+        (new EmployeePayrollData()).name = name.value;
+        textError.textContent = "";
+      } 
+      catch(e) {
+        textError.textContent = e;
+      }
+    });
+  
+    const startDateError = document.querySelector('.startDate-error');
+    const day  = document.querySelector('#day');
+    const month  = document.querySelector('#month');
+    const year  = document.querySelector('#year');
+    const dateSelect = document.querySelector('.select-div');
+    dateSelect.addEventListener('input', function(){
+      const date = new Date(day.value+"/"+month.value+"/"+year.value);
+      try{
+        (new EmployeePayrollData()).startDate = date;
+        startDateError.textContent = "";
+      } 
+      catch(e) {
+        startDateError.textContent = e;
+      }
     })
-}
-let EmpWage =calculateWage(totalEmpHrs);
-console.log("EMPLOYEE HOURS IS : "+totalEmpHrs+" hr\nAND TOTAL WORKING DAYS : " + totalWorkingDays + "\nAND EMPLOYEE WAGE IS : " + EmpWage);
-let totalEmpWage = 0;
-
-//MAP
-function totalWages(totalWage,dailyWage)
-{
-    return totalWage+dailyWage
-}
-console.log("Employee wage map totalHours: "+Array.from(employeeDailyWageMap.values()).reduce(totalWages,0))
-
-
-function sum(dailyWage) {
-    return totalEmpWage += dailyWage; 
-}
-empDailyWageArr.forEach(sum);
-console.log("\nCALCULATING TOTAL WAGE BY FOREACH METHOD : ")
-console.log("EMPLOYEE HOURS IS : "+totalEmpHrs+" hr\nAND TOTAL WORKING DAYS : " + totalWorkingDays + "\nAND EMPLOYEE WAGE IS : " + totalEmpWage);
-
-
-function totalWages(totalWage, dailyWage) {
-    return totalWage + dailyWage;
-}
-console.log("TOTAL EMPLLOYEE WAGE(REDUCE METHOD): " + empDailyWageArr.reduce(totalWages, 0) );
-let dailyCounter = 0;
-
-
-function mapDayWithWage(dailyWage) {
-    dailyCounter ++;
-    return "Day-" + dailyCounter + "-Wage is = "+ dailyWage;
-}
-let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
-console.log("DAILY WAGE MAP :");
-console.log(mapDayWithWageArr);
-
-function fulltimeWage(dailyWage) {
-    return dailyWage.includes("160");
-}
-let fulltimeArr = mapDayWithWageArr.filter(fulltimeWage);
-console.log("DAILY WAGE WHEN FULL TIME WAS EARNED");
-console.log(fulltimeArr);
-console.log("FIRST FULL TIME WAGE WAS EARNED: ");
-console.log(mapDayWithWageArr.find(fulltimeWage));
-
-function isAllFulltimeWage(dailyWage) {
-    return dailyWage.includes("160");
-}
-console.log("CHECKING ALL FULL TIME WAGE ELEMENTS:-")
-console.log(fulltimeArr.every(isAllFulltimeWage));
-
-
-function isAnyPartTimeWage(dailyWage) {
-    return dailyWage.includes("80");
-}
-console.log("CEHCKING IF ANY PART TIME WAGE :");
-console.log(mapDayWithWageArr.some(isAnyPartTimeWage));
-
-
-function totalDaysworked(numOfDays, dailyWage) {
-    if (dailyWage>0) return numOfDays+1;
-    return numOfDays;
-}
-console.log("NUMBER OF DAYS THE EMPLOYEE WORKED:")
-console.log(empDailyWageArr.reduce(totalDaysworked, 0));
-
-
-const findTotal = (totalVal, dailyVal) => {
-    return totalVal + dailyVal;
-}
-
-let totalHours = Array.from(empDailyHrsMap.values())
-                .filter(empHrs => empHrs > 0)
-                .reduce(findTotal, 0);
-let totalSalary = empDailyWageArr
-                .filter(dailyWage => dailyWage > 0)
-                .reduce(findTotal, 0);
-
-console.log(`TOTAL HOURS: ${totalHours}, TOTAL SALARY: ${totalSalary}`);
-
-let nonWorkingDays = new Array();
-let partWorkingDays = new Array();
-let fullWorkingDays = new Array();
-
-empDailyHrsMap.forEach((value, key, map) => {
-    if(value==4) partWorkingDays.push(key);
-    else if(value==8) fullWorkingDays.push(key);
-    else nonWorkingDays.push(key);
-});
-
-console.log("Full working days: "+ fullWorkingDays);
-console.log("Part working days: "+ partWorkingDays);
-console.log("Non working days: "+ nonWorkingDays);
-
-console.log("Showing daily hours and wage: "+empDailyHrsAndWageArray);
-
-totalSalary = empDailyHrsAndWageArray
-                .filter(empDailyHrsAndWage => empDailyHrsAndWage.dailyWage > 0) 
-                .reduce((totalWage, dailyHrsAndWage) => totalWage+= dailyHrsAndWage.dailyWage, 0);
-
-totalHours = empDailyHrsAndWageArray
-                .filter(empDailyHrsAndWage => empDailyHrsAndWage.dailyHours > 0) 
-                .reduce((totalHours, dailyHrsAndWage) => totalHours+= dailyHrsAndWage.dailyHours, 0);
-
-console.log(`Total Hours: ${totalHours}, total Salary: ${totalSalary}`);
-
-console.log("Logging full working days: ");
-empDailyHrsAndWageArray.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 8)
-                       .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()));
-
-partWorkingDays = empDailyHrsAndWageArray
-                  .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 4)
-                  .map(dailyHrsAndWage => dailyHrsAndWage.toString());
-
-console.log("\nPart Working days string "+partWorkingDays);
-
-nonWorkingDays = empDailyHrsAndWageArray
-                  .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
-                  .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
-
-console.log("Non Working days string "+nonWorkingDays);
+      
+    const salary = document.querySelector('#salary');
+    const output  = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+      output.textContent = salary.value;
+    });
+  });
+  
+  
+  const save = () => {
+    try{
+      let employeePayrollData = createEmployeePayroll();
+      
+    }
+    catch(e){
+      return;
+    }
+  }
+  
+  const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    const textError = document.querySelector('.text-error');
+    try{
+      employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+      textError.textContent = e;
+      throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender').pop();
+    employeePayrollData.departments = getSelectedValues('[name=department');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.notes = getInputValueById('#notes');
+  
+    const startDateError = document.querySelector('.startDate-error');
+    let date = getInputValueById('#day')+"/"+ getInputValueById('#month')+"/"+ getInputValueById('#year');
+    try {
+      employeePayrollData.startDate = new Date(date);
+    }
+    catch(e){
+      startDateError.textContent = e;
+      throw e;
+    }
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+  }
+  
+  const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+      if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+  }
+  
+  const getInputValueById = (id) => {
+    let value  = document.querySelector(id).value;
+    return value;
+  }
+  
+  const getInputElementValue =(id) => {
+    return document.getElementById(id).value;
+  }
