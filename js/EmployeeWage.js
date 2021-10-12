df7b274a4d8765bbf0a565f9f3ede9d7a8b34bde
namancg <naman.c@perfios.com>
2021-10-13 00:02:09 +0530

    REFACTORED: added event listeners when page is loaded

--- js/EmployeeWage.js
+++ js/EmployeeWage.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
+    //event listeners
     name.addEventListener('input', function() {
       if(name.value.length == 0){
         textError.textContent = "";
