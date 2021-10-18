6d086fdd41367ba3399aa4f9e24533a75d08b657
namancg <naman.c@perfios.com>
2021-10-19 00:44:02 +0530

    REFACTORED: method to remove employee data from json server

--- js/home.js
+++ js/home.js
@@ -71,9 +71,20 @@ const remove = (node) => {
     const index = empPayrollList.map(empData => empData.id)
                                 .indexOf(empPayrollData.id);
     empPayrollList.splice(index,1);
-    localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
-    document.querySelector(".emp-count").textContent = empPayrollList.length;
-    createInnerHtml();
+    if(site_properties.use_local_storage.match("true")){
+        localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+        createInnerHtml();
+    }
+    else{
+        const deleteUrl = site_properties.server_url+ empPayrollData.id.toString();
+        makeServiceCall("DELETE",deleteUrl, false)
+            .then(responseText => {
+                createInnerHtml();
+            })
+            .catch(error => {
+                console.log("DELETE Error Status: "+ JSON.stringify(error));
+            })
+    }
 }
 
 const update = (node) => {
