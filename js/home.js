e1b80e9a24cf9e05e27ea24503d8da6ff288929d
namancg <naman.c@perfios.com>
2021-10-19 00:29:16 +0530

    REFACTORED: link to http_service file

--- js/home.js
+++ js/home.js
@@ -1,23 +1,43 @@
 let empPayrollList;
-window.addEventListener('DOMContentLoaded', (event) => {
-    empPayrollList = getEmployeePayrollDataFromStorage();
+window.addEventListener('DOMContentLoaded',(event) =>{
+    if(site_properties.use_local_storage.match("true")){
+        getEmployeePayrollDataFromStorage();
+    }
+    else getEmployeePayrollDataFromServer();
+});
+
+const processEmployeePayrollDataResponse = () => {
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
     localStorage.removeItem('editEmp');
-});
+}
 
 const getEmployeePayrollDataFromStorage = () => {
-    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+    empPayrollList =  localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+    processEmployeePayrollDataResponse();
+}
+
+const getEmployeePayrollDataFromServer = () => {
+    makeServiceCall("GET", site_properties.server_url, true)
+        .then(responseText => {
+            empPayrollList = JSON.parse(responseText);
+            processEmployeePayrollDataResponse();
+        })
+        .catch(error => {
+            console.log("GET Error Status: "+ JSON.stringify(error));
+            empPayrollList = [];
+            processEmployeePayrollDataResponse();
+        })
 }
 
 const createInnerHtml = () => {
-    if (empPayrollList.length == 0) {
-        document.querySelector('#table-display').innerHTML = `<h2 class="no-emp-notification">Oops... No employees</h2>`;
+    if(empPayrollList.length == 0){
+        document.querySelector('#table-display').innerHTML = `<h2 class="no-emp-notification">No employees</h2>`;
         return;
-    }
+    } 
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
     let innerHtml = `${headerHtml}`;
-    for (const empPayrollData of empPayrollList) {
+    for (const empPayrollData of empPayrollList){
         console.log(empPayrollData);
         innerHtml = `${innerHtml}
         <tr>
@@ -39,7 +59,7 @@ const createInnerHtml = () => {
 
 const getDeptHtml = (deptList) => {
     let deptHtml = '';
-    for (const dept of deptList) {
+    for(const dept of deptList){
         deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
     }
     return deptHtml;
@@ -47,18 +67,18 @@ const getDeptHtml = (deptList) => {
 
 const remove = (node) => {
     let empPayrollData = empPayrollList.find(empData => empData.id == node.id);
-    if (!empPayrollData) return;
+    if(!empPayrollData) return;
     const index = empPayrollList.map(empData => empData.id)
-        .indexOf(empPayrollData.id);
-    empPayrollList.splice(index, 1);
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+                                .indexOf(empPayrollData.id);
+    empPayrollList.splice(index,1);
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
 }
 
 const update = (node) => {
     let empPayrollData = empPayrollList.find(empData => empData.id == node.id);
-    if (!empPayrollData) return;
-    localStorage.setItem('editEmp', JSON.stringify(empPayrollData));
+    if(! empPayrollData) return;
+    localStorage.setItem('editEmp',JSON.stringify(empPayrollData));
     window.location.replace(site_properties.add_emp_payroll_page);
-}
+} 
